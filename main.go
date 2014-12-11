fac85788d9d0c782c0dbf2afeac1da5d755c5549
Brandon Hilkert <brandonhilkert@gmail.com>
2014-12-11 09:18:26 -0500

    Fix redis with dotenv

--- main.go
+++ main.go
@@ -3,18 +3,12 @@ package main
 import (
 	"github.com/brandonhilkert/pipeline-activity/pubsub"
 	"github.com/brandonhilkert/pipeline-activity/socket"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	redisUrl := os.Getenv("REDIS_URL")
 
 	l := pubsub.NewListener(redisUrl)
